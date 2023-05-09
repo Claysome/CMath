a7bcce88b12a832820d8dd035caed12cf7ee8227
clay <claysomes@outlook.com>
2023-05-10 01:54:28 +0800

    demo

---
+++ build/CMakeFiles/CMath.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CMath.dir/test.cpp.o"
+  "CMath.pdb"
+  "CMath"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/entrance.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for entrance.
+# This may be replaced when dependencies are built.
